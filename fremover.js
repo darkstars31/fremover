d972e387ecd7687e44dcab953f29ad7ea5179250
Anthony Santi <darkstars31@gmail.com>
2019-10-28 10:25:03 -0500

    Updated javascript implementation to use a promise and better console output

--- fremover.js
+++ fremover.js
@@ -21,22 +21,35 @@ getFilesNamesThatMatchExpression = (dir, matchArray, files_) => {
    return files_;
 }
 
+removeFitAndFdescribes = (file) => {
+    return new Promise((resolve,reject) => {
+        fs.readFile(file,(err,data) => {
+            let fileContents = data.toString('utf8');
+            let fitMatch = new RegExp(/fit\(/gi);
+            let fdescribeMatch = new RegExp(/fdescribe\(/gi);
+            if(fitMatch.test(fileContents) || fdescribeMatch.test(fileContents)){   
+                filesModified++;
+                console.log(`Fixed file: ${file}`) 
+                var updatedFile = fileContents.replace(fitMatch, 'it(').replace(fdescribeMatch, 'describe(');
+                fs.writeFile(file, updatedFile, err => {
+                    if(err){ console.log(err); }
+                });
+            }  
+            return resolve();
+        });      
+    });
+}
+
+outputStats = () => {
+    console.log(`${filesModified} files modified.`); 
+}
+
 var filesModified = 0;
 var fileList = getFilesNamesThatMatchExpression(__dirname+"\\src", [match1, match2]);
-console.log(`${fileList.length} Files Located`)
-for(let i = 0; i < fileList.length; i++){
-    let file = fileList[i];
-    fs.readFile(file,(err,data) => {
-        let fileContents = data.toString('utf8');
-        let fitMatch = new RegExp(/fit\(/gi);
-        let fdescribeMatch = new RegExp(/fdescribe\(/gi);
-        if(fitMatch.test(fileContents) || fdescribeMatch.test(fileContents)){   
-            console.log(`File: ${file}`) 
-            var updatedFile = fileContents.replace(fitMatch, 'it(').replace(fdescribeMatch, 'describe(');
-            fs.writeFile(file, updatedFile, err => {
-                if(err){ console.log(err); }
-            });
-        }  
-    });  
-}
-console.log(`${filesModified} files modified.`); 
+console.log(`fremover - swatting those pesky f's hidden in your tests. Your builds should feel liberated.`);
+console.log(`Written w/ <3 by Tony`);
+console.log(`${fileList.length} Files Located`);
+Promise.all(fileList.map(file => removeFitAndFdescribes(file))
+.then(() => {
+    outputStats();
+}));
